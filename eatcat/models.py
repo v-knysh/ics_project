63d0a30d97cc4fe3bcd563f98428a5000584f8dd
Vlad Knysh <knyshv2412@gmail.com>
2017-12-20 18:05:12 +0200

    added initial views

--- eatcat/models.py
+++ eatcat/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Cat:
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+    def __str__(self):
+        return f"Cat {self.name}"
+
+    def __repr__(self):
+        return f"Cat {self.name}"
--- eatcat/views.py
+++ eatcat/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
+from eatcat.models import Cat
 # Create your views here.
+
+def cats_list(request):
+    cats = [Cat(f"Cat{i}", 5+i/2)for i in range(10)]
+    return HttpResponse(json.dumps(cats, default=lambda o: o.__dict__, sort_keys=True, indent=4))
+
+def cat_info(request, cat_id):
+    cat = Cat(f"Cat{cat_id}", 5+cat_id/2)
+    return HttpResponse(json.dumps(cat, default=lambda o: o.__dict__, sort_keys=True, indent=4))
