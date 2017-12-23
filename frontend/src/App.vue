d44eb20c5b626e0e17a14ebca55eb0eef1fd9c24
Denis Tsymbal <tsimbal1311@gmail.com>
2017-12-23 14:59:54 +0200

    Add regestration and router for dashboard

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,13 +5,16 @@
 
 			<ul class="navbar-nav ml-auto">
 				<li class="nav-item" v-if="isLogin">
-					<router-link class="nav-link" to="/dashboard">Зайти до кабінета, {{ username }}</router-link>
+					<a class="nav-link" href="#" @click="goDashboard">Зайти до кабінета, {{ username }}</a>
 				</li>
 				<li class="nav-item" v-if="isLogin">
 					<button class="btn btn-danger" href="#" @click="logout">Вийти з кабінету</button>
+				</li>			
+				<li class="nav-item" v-if="!isLogin">
+					<router-link class="nav-link" to="/about" >Про нас</router-link>
 				</li>
 				<li class="nav-item" v-if="!isLogin">
-					<a class="nav-link" href="#" >Регестрація</a>
+					<a class="nav-link" data-toggle="modal" data-target="#reg-modal" href="#" >Регестрація</a>
 				</li>
 				<li class="nav-item" v-if="!isLogin">
 					<button class="btn btn-outline-light" data-toggle="modal" data-target="#login-modal" href="#">Вхід до кабінета</button>
@@ -25,7 +28,7 @@
 				<div class="modal-content">
 					<div class="modal-header">
 						<h5 class="modal-title">Вхід до кабінету</h5>
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close" ref="closeLogin">
 							<span aria-hidden="true">&times;</span>
 						</button>
 					</div>
@@ -46,7 +49,45 @@
 						</form>
 					</div>
 					<div class="modal-footer">
-						<button type="submit" class="btn btn-primary" id="submit-form" data-dismiss="modal"  @click="submitLogin">Ввійти</button>
+						<button type="submit" class="btn btn-primary" id="submit-form" :data-dismiss="modal"  @click="submitLogin">Ввійти</button>
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Закрити</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="modal" id="reg-modal" tabindex="-1" role="dialog">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title">Реєстрація</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close" ref="closeReg">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<form>
+							<div class="form-group row">
+								<label for="newlogin" class="col-sm-2 col-form-label">Логін</label>
+								<div class="col-sm-10">
+									<input type="text" class="form-control" id="newlogin" v-model="newlogin" placeholder="Логін">
+								</div>
+							</div>
+							<div class="form-group row">
+								<label for="email" class="col-sm-2 col-form-label">Email</label>
+								<div class="col-sm-10">
+									<input type="text" class="form-control" id="email" v-model="email" placeholder="Email">
+								</div>
+							</div>
+							<div class="form-group row">
+								<label for="newpassword" class="col-sm-2 col-form-label">Пароль</label>
+								<div class="col-sm-10">
+									<input type="password" class="form-control" id="newpassword" v-model="newpassword" placeholder="Пароль">
+								</div>
+							</div>
+						</form>
+					</div>
+					<div class="modal-footer">
+						<button type="submit" class="btn btn-primary" id="submit-form" :data-dismiss="modal"  @click="regButton">Зареєструвати</button>
 						<button type="button" class="btn btn-secondary" data-dismiss="modal">Закрити</button>
 					</div>
 				</div>
@@ -64,30 +105,76 @@ export default {
 			isLogin: false,
 			login: '',
 			password: '',
-			username: ''
+			newlogin: '',
+			newpassword:'',
+			email:'',
+			username: '',
+			modal:'',
+			pk: ''
 		}
 	},
 	methods: {
 		submitLogin: function(event) {
+			let close = this.$refs.closeLogin;
 			let data = {
 				login: this.login,
-				pass: this.password
+				password: this.password
 			}
 
-		 this.$http.post('http://localhost:8000/users', data).then(response => {
-		 		console.log(response); 
-		 		this.username = response.body.user
-		    this.isLogin = true
-		  }, err => {
-		  	console.log(err)
-		  })
+			this.$http.post('http://localhost:8000/users', data).then(response => {
+				localStorage.setItem('pk', response.body.pk)
+				localStorage.setItem('username', response.body.user)
+				this.username = response.body.user
+				this.isLogin = true
+				close.click()
+			}, err => {
+				alert(err.msg)
+			})
+		},
+		goDashboard: function(event) {
+			let data = {
+				pk: localStorage.getItem('pk')
+			}
+			this.$http.post('http://localhost:8000/', data).then(response => {
+				this.$router.push('/dashboard/')
+			}, err => {
+				alert(err.msg)
+			})
+		},
+		regButton: function(event) {
+			let close = this.$refs.closeReg;
+			let data = {
+				login: this.newlogin,
+				email: this.email,
+				password: this.newpassword
+			}
+			this.$http.post('http://localhost:8000/reg', data).then(response => {
+				alert(`Заэрестровано користувача ${this.newlogin}`)
+				close.click();
+			}, err => {
+				console.log(err); 
+			});
 		},
 		logout: function() {
-			this.$router.push('/')
+			localStorage.removeItem('pk')
 			this.isLogin = false
-			username = ''
-		}
-	}
+			this.$router.push('/')
+		},
+		checkoutLogin: function() {
+			let jwt = localStorage.getItem('pk')
+			if(jwt) {
+				this.username = localStorage.getItem('username')
+				this.isLogin = true
+			}
+			else {
+				this.isLogin = false
+				this.$router.push('/')  
+			}
+		},
+	},
+	created: function () {
+		this.checkoutLogin();
+	} 
 
 }
 </script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,10 @@ import Resource from 'vue-resource';
 import Router from 'vue-router'
 import Main from '@/components/Main'
 import Dashboard from '@/components/Dashboard'
+import AboutCat from '@/components/AboutCat'
+import Plan from '@/components/Plan'
+import Refrigerator from '@/components/Refrigerator'
+import About from '@/components/About'
 
 Vue.use(Router)
 Vue.use(Resource);
@@ -11,16 +15,36 @@ Vue.http.headers.common['Content-Type'] = 'application/json'
 // Vue.http.headers.common['Access-Control-Allow-Origin'] = '*'
 
 export default new Router({
+  linkActiveClass: 'active',
   routes: [
+  {
+    path: '/',
+    name: 'Main',
+    component: Main
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
+    children: [
     {
-      path: '/',
-      name: 'Main',
-      component: Main
+      path: 'about',
+      component: AboutCat
     },
     {
-      path: '/dashboard',
-      name: 'Dashboard',
-      component: Dashboard
-    }
+      path: 'refrigerator',
+      component: Refrigerator
+    },
+    {
+      path: 'plan',
+      component: Plan
+    },
+    ]
+  }
   ]
 })
